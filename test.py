bd8a874a28b9b9c1a6a0ac3beebbdddcae7e003c
Vadim Mishanin <v.mishanin@takeprofit.com>
2026-02-04 21:55:48 +0800

    Handling input arguments

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import example
+
 # Invoke C++ functions:
 example.foo()
 example.bar()
-print('print C++ string:', example.greeting())
-print('speed of light', example.speed_of_light())
+print("print C++ string:", example.greeting())
+print("pi", example.pi())
+print("speed of light", example.speed_of_light())
+print("is_zero", example.is_zero(12.23))
+print("sum", example.sum(3, 124))
+print("div", example.div(10.0, 3.0))
