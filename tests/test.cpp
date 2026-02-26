e36c4f3e18e287e41eac4e9fa3da5ad5a8ff8b0e
blacktea <42189825+blacktea@users.noreply.github.com>
2026-02-27 00:57:40 +0800

    Feature/library (#3)
    * rearrange files
    
    * Added pytests
    
    ---------
    
    Co-authored-by: Vadim Mishanin <v.mishanin@takeprofit.com>

---
+++ tests/test.cpp
@@ -0,0 +1,25 @@
+#include "rebind/rebind.hpp"
+
+#include <print>
+
+namespace test {
+
+int a{};
+
+void foo() { std::println("foo method"); }
+
+void bar() { std::println("bar method"); }
+
+std::string greeting() { return "hello from C++ with Reflection. This's cool feature! C++ ❤Python"; }
+
+float pi() { return 3.14f; }
+
+long long speed_of_light() { return 300'000'000; }
+
+int sum(int a, int b) { return a + b; }
+
+bool is_zero(float f) { return f <= 0.001; }
+
+}  // namespace test
+
+REFLB_MODULE(tests, test)
